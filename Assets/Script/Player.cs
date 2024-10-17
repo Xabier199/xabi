5fd3a5fe9534b790675b1a76fe9953d10f2e2f44
Xabier Galet <galet.xabier@uni.eus>
2024-10-17 14:28:22 +0200

    cambio2

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -11,29 +11,45 @@ public class PLayerMovement : MonoBehaviourPunCallbacks
     private float jumpforce = 1.0f;
     private bool grounded;
     public float velocidad;
+    private Animator animator;
+
 
     void Start()//Coger el RigidBody del objeto al que esté asignado el script
     {
+
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         if (photonView.IsMine)
         {
+            //Girar al jugador
+            if (horizontal < 0)
+                transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+            else if (horizontal > 0)
+                transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+
+
             horizontal = Input.GetAxis("Horizontal");// Coger el input del teclado, con valores del -1 al 1
 
-            Debug.DrawRay(transform.position, Vector2.down, Color.red);
-            RaycastHit2D hitGround = Physics2D.Raycast(transform.position, Vector2.down, 0.1f);
 
+            Debug.DrawRay(transform.position, Vector2.down * 2.5f, Color.red);
+            RaycastHit2D hitGround = Physics2D.Raycast(transform.position, Vector2.down, 2.5f);
             if (hitGround) grounded = true;
             else grounded = false;
 
-            if ((Input.GetKeyDown(KeyCode.Space)) || (Input.GetKeyDown(KeyCode.W)) && grounded)//GetkeyDown quiere decir cuando presionas una tecla, en este caso con KeyCode hemos puesto el espacio
+            if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) && grounded)//GetkeyDown quiere decir cuando presionas una tecla, en este caso con KeyCode hemos puesto el espacio
             {
                 Jump();
             }
+
         }
+
+
+
+
     }
     private void FixedUpdate()
     {
--- Assets/Script/SpawnPlayers.cs
+++ Assets/Script/SpawnPlayers.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Unity.VisualScripting;
 
 public class SpawnPlayers : MonoBehaviourPunCallbacks
 {
 
 
-    public PhotonView playerPrefab;
+    public PhotonView playerPrefab1;
+    public PhotonView playerPrefab2;
+
+    public int cont =0;
 
 
     public Transform spawnPoint;
@@ -19,17 +23,32 @@ public class SpawnPlayers : MonoBehaviourPunCallbacks
         // PhotonNetwork.ConnectUsingSettings();
         Debug.Log("conectado al room");
 
+        if ((cont%2) == 1)
+        {
+            PhotonNetwork.Instantiate(playerPrefab1.name, spawnPoint.position, spawnPoint.rotation);
+        }
+        else
+        {
+            PhotonNetwork.Instantiate(playerPrefab2.name, spawnPoint.position, spawnPoint.rotation);
+        }
 
-        PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, spawnPoint.rotation);
+       
     }
 
-   // public override void OnConnectedToMaster()
-   // {
-  //      Debug.Log("conectado al Master");
-  //      PhotonNetwork.JoinRandomOrCreateRoom();
-  //  }
+    void OnPhotonInstantiate()
+    {
+        // e.g. store this gameobject as this player's charater in Player.TagObject
+        cont = cont + 1;
+       // cont = cont % 2;
+    }
 
-   // public override void OnJoinedRoom()
+    // public override void OnConnectedToMaster()
+    // {
+    //      Debug.Log("conectado al Master");
+    //      PhotonNetwork.JoinRandomOrCreateRoom();
+    //  }
+
+    // public override void OnJoinedRoom()
     //{
     //    Debug.Log("conectado al room");
 
@@ -37,7 +56,7 @@ public class SpawnPlayers : MonoBehaviourPunCallbacks
     //    PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, spawnPoint.rotation);
 
 
-   // }
+    // }
 
 
 }
